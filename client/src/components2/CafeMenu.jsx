e35019571441e16560720944e4e0fbdad360cdf6
Arpita <arpitapawar1608@gmail.com>
2026-01-21 10:31:16 +0530

    new changes in react application

---
+++ client/src/components2/CafeMenu.jsx
@@ -0,0 +1,54 @@
+import "../../dummy/styles.css"
+export const CafeMenu=()=>{
+    return(<div className="menu">
+      <main>
+        <h1>CAMPER CAFE</h1>
+        <p className="established">Est. 2020</p>
+        <hr/>
+        <section>
+          <h2>Coffee</h2>
+          <img src="https://cdn.freecodecamp.org/curriculum/css-cafe/coffee.jpg" alt="coffee icon"/>
+          <article className="item">
+            <p className="flavor">French Vanilla</p><p className="price">3.00</p>
+          </article>
+          <article className="item">
+            <p className="flavor">Caramel Macchiato</p><p className="price">3.75</p>
+          </article>
+          <article className="item">
+            <p className="flavor">Pumpkin Spice</p><p className="price">3.50</p>
+          </article>
+          <article className="item">
+            <p className="flavor">Hazelnut</p><p className="price">4.00</p>
+          </article>
+          <article className="item">
+            <p className="flavor">Mocha</p><p className="price">4.50</p>
+          </article>
+        </section>
+        <section>
+          <h2>Desserts</h2>
+          <img src="https://cdn.freecodecamp.org/curriculum/css-cafe/pie.jpg" alt="pie icon"/>
+          <article className="item">
+            <p className="dessert">Donut</p><p className="price">1.50</p>
+          </article>
+          <article className="item">
+            <p className="dessert">Cherry Pie</p><p className="price">2.75</p>
+          </article>
+          <article className="item">
+            <p className="dessert">Cheesecake</p><p className="price">3.00</p>
+          </article>
+          <article className="item">
+            <p className="dessert">Cinnamon Roll</p><p className="price">2.50</p>
+          </article>
+        </section>
+      </main>
+      <hr className="bottom-line"/>
+      <footer>
+        <address>
+          <p>
+            <a href="https://www.freecodecamp.org" target="_blank">Visit our website</a>
+          </p>
+          <p className="address">123 Free Code Camp Drive</p>
+        </address>
+      </footer>
+    </div>)
+}
---
+++ client/src/components2/sample.js
@@ -0,0 +1,10 @@
+function add(a,b,c)
+{
+c=a+b;
+console.log(c);
+}
+add(5,10)
+const app=()=>
+{
+return
+}
